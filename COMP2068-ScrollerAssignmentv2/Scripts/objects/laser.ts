2e6ff684d0a41b211dc0449732e614d2e6ca6b69
chutch01 <chutch01@hotmail.com>
2015-03-23 13:57:10 -0400

    started basics for the functions of the laser

--- COMP2068-ScrollerAssignmentv2/Scripts/objects/laser.ts
+++ COMP2068-ScrollerAssignmentv2/Scripts/objects/laser.ts
@@ -4,27 +4,18 @@
 
         // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++
         constructor() {
-            super("enemy");
-            this.name = "enemy";
+            super("laser");
+            this.name = "laser";
             this.soundString = "explosion";
+            this._dx = 5;
 
-            this._reset();
 
         }
 
         // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++
-        private _reset() {
-            // set the island to start at a random x value
-            this.y = Math.floor(Math.random() * constants.SCREEN_HEIGHT);
-            this.x = Math.floor(Math.random() * constants.SCREEN_HEIGHT) + constants.SCREEN_WIDTH;
-            // add drift to the cloud 
-            //this._dy = Math.floor(Math.random() * 5) + 5;
-            this._dx = -5;
-        }
 
         private _checkBounds() {
-            if (this.x <= 0 - this.width) {
-                this._reset();
+            if (this.x >= 680 + this.width) {
             }
         }
 
